4b914e8e0e065d0e8347cde22e58da55f54ba938
lenny-j <88339503+lenny-j@users.noreply.github.com>
2022-05-11 17:36:43 -0500

    add lesson 1 file(s)

---
+++ Lesson-1-Meet_PowerShell_Tools/Meet-PowerShell.ps1
@@ -0,0 +1,29 @@
+########
+# Lesson 1: Meet PowerShell!
+########
+
+### Your PowerShell tools ###
+
+# 1 - Console
+# Search for: "Powershell"
+
+# 2 - Editor - Integrated Scripting Environment
+# Search for: "PowerShell ISE"
+
+### Hey PowerShell - Tell Me How Cool I Am ###
+
+# Note: this won't return any results if you haven't copied something into your clipboard
+
+Get-Clipboard
+
+
+### Hey PowerShell - Copy This File ###
+
+Copy-Item "CoolFile.txt" -Destination "c:\temp\"
+
+
+### Hey PowerShell - Say Hello to Everyone ###
+
+Write-Host "Hello World!"
+
+
